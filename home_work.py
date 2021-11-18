a8a7ed4aef99c9dd67e78a650656a5a72c378df9
nasirdinozavrr <94150761+nasirdinozavrr@users.noreply.github.com>
2021-11-18 21:53:25 +0600

    home_work.1.1

--- home_work.py
+++ home_work.py
@@ -1,9 +1,18 @@
-def algorithm(numbers, desired_sum):
-    for n in numbers:
-        for d in numbers[numbers.index(n)+1:]:
-            if n+d==desired_sum:
-                return(numbers.index(n),numbers.index(d))
+class Algorithm:
+    def __init__(self, numbers, desired_sum):
+        self.numbers = numbers
+        self.desired_sum = desired_sum
+
+    def print_algorithm(self):
+        for n in self.numbers:
+            for d in self.numbers[self.numbers.index(n) + 1:]:
+                if n + d == self.desired_sum:
+                    return (self.numbers.index(n),self.numbers.index(d))
+
+
+algorithm = Algorithm(numbers=[2, 7, 11, 15], desired_sum=9)
 
 
 
-print(algorithm([2, 7, 11, 15], 9))
+
+print(algorithm.print_algorithm())
